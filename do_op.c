e3e9cdb4c298e55e79c4723d0dc4570dc3cda85b
Badrinath R <r.badrinath@gmail.com>
2022-09-16 14:18:24 +0530

    doing the expression right

--- do_op.c
+++ do_op.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 int main(){
-	printf("Give me two ints and a character");
+	printf("Give me a simple arithmetic operation on integers "
+			"like 4 - 3:\n");
 	int i1, i2;
-	scanf("%d %d ",&i1,&i2);
+	//scanf("%d %d ",&i1,&i2);
+	//char c = getchar();
+	scanf("%d ",&i1);
 	char c = getchar();
+	scanf("%d",&i2);
 	switch(c){
-		case '+': printf("%d",i1+i2);
+		case '+': printf("%d\n",i1+i2);
 			  break;
-		case '-': printf("%d",i1-i2);
+		case '-': printf("%d\n",i1-i2);
 			  break;
-		default: printf("not implemented\n");
+		case '*': printf("%d\n",i1*i2);
+			  break;
+		case '/': printf("%d\n",i1/i2);
+			  break;
+		default: printf("operator %c not implemented\n", c);
 	}
 }
