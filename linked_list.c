0e3507af391444a8403e199c22e17f878b042925
Badrinath R <r.badrinath@gmail.com>
2022-10-10 15:52:18 +0530

    added struct examples

---
+++ linked_list.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Here is an interesting structure
+struct list {
+	int val;
+	struct list * next;
+};
+// The above has two members one of which is a pointer type.
+
+// Below is a simple funciton to get a new "struct list" node
+struct list * new_node(){
+	return (struct list *) malloc(sizeof(struct list));
+}
+
+// Here is how one struct list node can have its "next" field point 
+// to another node.
+void join_next(struct list *node1, struct list *node2){
+	node1->next=node2;
+}
+
+// For fun here is a function that simply adds 10 new nodes one
+// after the other to the end of a given node called node1.
+void join_n_new(struct list *node1, int n){
+	struct list * p=node1;
+	for (int i; i <n ; i++){
+		p->next=new_node();
+		p=p->next;
+	}
+	// what is p at the end?
+}
+
+
+// Can you write a function to simply go throu all nodes in the linked list 
+// and set the val field to some number, say 100.
+void set_list_values( struct list * node){
+}
+
+// Can you write a function to read from the user values and add them
+// as new nodes into an existing list who's only node in the beginning
+// is given?
+void read_list_values( struct list * node){
+}
---
+++ simple_struct.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// This is a common way to define a new aggregate type called a structure
+struct complex {
+	float real;
+	float imaginary;
+};
+// The above structure has two "field"s or "member"s called "real" and
+// "imaginary" each of them is as good as a separate variable
+// but clubbed together into one larger variable... we'll see an example
+
+
+// Here is another interesting structure
+struct list {
+	int v;
+	struct list * next;
+};
+// The above has two members one of which is a pointer type.
+
+//
+void read_complex( struct complex * v){
+	scanf("%f %f",&(*v).real, &(*v).imaginary);
+}
+void print_complex( const struct complex * v){
+	printf("%f %f",(*v).real, (*v).imaginary);
+}
+
+void add_complex( const struct complex * u, 
+		  const struct complex * v, struct complex *w){
+	(*w).real =  (*u).real+(*v).real;
+	(*w).imaginary = (*u).imaginary +(*v).imaginary ;
+}
+
+// note that (*v).real and v->real are the same
+void print_2_complex( const struct complex * v){
+	printf("%f %f",v->real, v->imaginary);
+}
+
+void print_3_complex( const struct complex v){
+	printf("%f %f",v.real, v.imaginary);
+}
+
+
+
+void main(){
+  struct complex a,b,c;
+
+  //1. The members of structures are access with the "." operator
+  a.real=5;
+  a.imaginary=10;
+  printf("%f %f\n",a.real,a.imaginary);
+
+  /*
+  //2. also we can have pointers to structures
+  struct complex * p;
+  p=&a;
+
+  //3. Note that *p is a structure so it has real and imaginary parts
+  printf("%f %f\n", (*p).real, (*p).imaginary);
+  // Note that we can replace (*p).real by p->real .. a C language convenience
+  
+  //4. We can also malloc structures
+  p=malloc(sizeof(struct complex));
+  p->real=55;
+  p->imaginary=100;
+  printf("%f %f\n", p->real, p->imaginary);
+  */
+
+  /*
+  //5. We can treat structure variables just like integers and other basic types:
+  printf("\nGive me real and imaginary parts of the first number:");
+  read_complex(&a);
+  printf("\nGive me real and imaginary parts of the second number:");
+  read_complex(&b);
+  add_complex(&a,&b,&c);
+  printf("\nReal and imaginary parts of the sum are:");
+  print_complex(&c);
+  */
+
+  //6. We can also create arrays of structures, just like basic types 
+  /*
+  struct complex x[3];
+  printf("\nGive me real and imaginary parts of the first number:");
+  read_complex(&x[0]);
+  printf("\nGive me real and imaginary parts of the second number:");
+  read_complex(&x[1]);
+  add_complex(&x[0],&x[1],&x[2]);
+  printf("\nReal and imaginary parts of the sum are:");
+  print_complex(&x[2]);
+  */
+
+  //7. We can also pass value (unlike arrays, but like other basic types)
+  /*
+  printf("\nReal and imaginary parts of the sum are (AGAIN):");
+  print_3_complex(&x[2]);
+  */
+
+   
+}
+
+
