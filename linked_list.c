20048be620a1a2927dd8d13ec0a55e1ad07be46a
Badrinath R <r.badrinath@gmail.com>
2022-10-13 13:46:44 +0530

    compile rror fixed

--- linked_list.c
+++ linked_list.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 
 // Here is an interesting structure
-struct list {
+struct node {
 	int val;
-	struct list * next;
+	struct node * next;
 };
 // The above has two members one of which is a pointer type.
 
-// Below is a simple funciton to get a new "struct list" node
-struct list * new_node(){
-	return (struct list *) malloc(sizeof(struct list));
+// Below is a simple funciton to get a new "struct node" node
+struct node * new_node(){
+	return (struct node *) malloc(sizeof(struct node));
 }
 
-// Here is how one struct list node can have its "next" field point 
+// Here is how one struct node node can have its "next" field point 
 // to another node.
-void join_next(struct list *node1, struct list *node2){
+void join_next(struct node *node1, struct node *node2){
 	node1->next=node2;
 }
 
 // For fun here is a function that simply adds 10 new nodes one
 // after the other to the end of a given node called node1.
-void join_n_new(struct list *node1, int n){
-	struct list * p=node1;
+void join_n_new(struct node *node1, int n){
+	struct node * p=node1;
 	for (int i; i <n ; i++){
 		p->next=new_node();
 		p=p->next;
@@ -31,15 +31,15 @@ void join_n_new(struct list *node1, int n){
 }
 
 
-// Can you write a function to simply go throu all nodes in the linked list 
-// and set the val field to some number, say 100.
-void set_list_values( struct list * node){
+// Can you write a function to simply go throu all nodes in the 
+// linked list of nodes and set the val field to some number, say 100.
+void set_list_values( struct node * node){
 }
 
 // Can you write a function to read from the user values and add them
 // as new nodes into an existing list who's only node in the beginning
 // is given?
-void read_list_values( struct list * node){
+void read_list_values( struct node * node){
 }
 
 void main() {
