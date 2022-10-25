82212c54c2e38fffe8746f818dfca49420d4eb76
Badrinath R <r.badrinath@gmail.com>
2022-10-25 22:08:01 +0530

    small-correction

--- matmul.c
+++ matmul.c
@@ -24,6 +24,7 @@ int  matread(float a[][10]){
 void matmul(float a[][10], float b[][10], float c[][10], int n){
   for (int i = 0;i < n; i++) {
      for (int j = 0;j < n; j++) {
+	c[i][j]=0;
         for (int k=0; k< n ; k++ )
 		c[i][j]+=a[i][k]*b[k][j];
      }
