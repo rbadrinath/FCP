9c1f43a7b73c9224b3349968a80a38f889bbd9d8
Badrinath R <r.badrinath@gmail.com>
2022-11-18 07:17:03 +0530

    more-examples

--- list_avg_w.c
+++ list_avg_w.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 
-int main(){
+void average_list(){
 	int i;
 	int sum=0;
 	int n;
 	int x;
-	printf("Howmany numbers to sum? ");
+
+	printf("  Howmany numbers to sum? ");
 	scanf("%d",&n);
-	printf("Enter %d numbers to sum ",n );
-	i=0;
+
+	printf("  Enter %d numbers to sum ",n );
+
+	i=0;   // count howmany integers already read
 	while (i < n ){
 		scanf("%d",&x);
-		sum+=x;
-		
-		i=i+1;
+		i=i+1;		// increment number of integers read
+
+		sum+=x;		// add read integer to sum
 	}
-	printf("Sum is %d\n",sum);
+	printf("  Sum is %d\n",sum);
+}
+
+void factorial(){
+	int n;
+	printf("  n =  ");
+	scanf("%d",&n);
+
+	int i=1;
+	int prod=1;
+	while (i <= n ) {
+		prod=prod*n;
+	}
+
+	printf("  Factorial is %d\n",prod);
+}
+
+void main(){
+	factorial();
+	// average_list();
 }
