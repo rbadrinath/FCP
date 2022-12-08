6587982eb41cd83d09242c29d15367ff7ec6de81
Badrinath R <r.badrinath@gmail.com>
2022-12-08 18:31:30 +0530

    updated fact

--- fact.c
+++ fact.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
+
+// An introductory idea of recursion. This is a well known function
+// Mathematically:
+//  factorial(n) :: 1                    if n equals  0
+//               :: n * factorial(n-1)   if n > 0 
+//  We dont define it for negative numbers
 int fact(int n){
-	int x;
-	if (n <= 1 )
+	if (n == 0 )
+		// we call this the base case or *non*-recursive case
 		return 1;
 	else
+		// we call this the recursive case
 		return n*fact(n-1);
+	// Draw a call tree to see what it looks like, then 
+	// draw a call stack. Also look at what gdb says
 }
-int test(int y){
-	int x=1;
-	int n = x + y; 
+int test(int n){
+	// This is a simple function just to illustrate what 
+	// the frame looks like for a normal function call
+	int x=10;
+	int n = x * n; 
 	return n;
 }
 int main(){
-	int v=5;
-	int f=test(v);
-	printf(" I got %d\n",f);
+	int n=0;
 	int z=3;
+	int v;
+	scanf("%d",&n);
+
+	v = test(n);  // replace with fact(n)
+	printf(" main got %d\n",v);
 }
