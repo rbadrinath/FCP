e2c0e5ed200c0f4364b46680be4bfb76c16954f5
Badrinath R <r.badrinath@gmail.com>
2022-12-09 14:52:23 +0530

    updatedexample

--- pointer_simple.c
+++ pointer_simple.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define TEST_NO 2
 
 // This file has multiple tests for the way pointers work.
 // Select the test by defining the variable test in main() to be in the appropriate range
@@ -8,70 +9,72 @@ void show_value_p(void *p){		// recall that a pointer holds an address value
 }
 
 void main(){
-	int test=3;
+	int test= TEST_NO ;
 
-	printf("Will do test # %d\n",test);
+	printf(" - - - - - - - - Will do test # %d\n",test);
 
-	// Unititialized p being used
-	// because p=something must happen before his, else p's usafe is not valid
-	// Right now p is some junk. So should give us 'Segmentation fault':
-	if ( test == 1 ) { 
+	switch(test){
+	     case 1: { 
+		// p is unititialized
 		int *p;
-		*p=1024;		// This step should result in 'Segmentation fault'
-		// Lets see the address that p holds
+		// Lets see the address that p holds - should be junk
 		printf("p holds the address %p\n", p);
-		// Lets see what is in the address that p holds
+		// The location p is looking at change that int's value to 1024
+		//   normally p=something must happen before his, 
+		*p=1024; // This step should result in 'Segmentation fault'
+		// Lets see what is in the address that p holds - should get here
 		printf("that above address has this value %d\n",*p);
-	}
+	      }
 	
-	// Now we are putting 1024 into the variable p which should hold addresses, not numbers.
-	// 1024 is not a valid address! So should give us a 'Segmentation fault'
-	if ( test == 2 ){
+	     case 2: {
+		// p has an invalid address - sort of like unitialized
 		int *p;
-		p = (int *) 1024; 
+		p = (int *) 1024; // Try putting 1024 into p .. not a valid address
 		// Lets see the address that p holds
 		printf("p holds the address %p\n", p);
 		// Lets see what is in the address that p holds
 		printf("that above address has this value:\n"); 
-		printf(" %d\n",*p);	// This step should result in 'Segmentation fault'
-	}
+		printf(" %d\n",*p); // This step should cause in 'Segmentation fault'
+	      }
 
-	// Now the right way to do things:
-	if ( test == 3 ){
+	      case 3: {
+		// Now the right way to do things:
 		int *p;
 		int i;
-		p = &i;		// This ensures the address in p is a valid integer location
-				// Note that i is un-initialized, which may be a problem if we use it's value
+		p = &i;  // This ensures the address in p is a valid integer location
+			 // Note that i is un-initialized, 
+			 // which may be a problem if we use it's value
 		// Lets see the address that p holds
 		printf("p holds the address %p\n", p);
 		// Lets see what is in the address that p holds
-		show_value_p(&i);	// The "&" operator actually results in an address value
+		show_value_p(&i);  // The "&" operator results in an address value
 		printf("that above address has this value:\n"); 
 		printf(" *p is  %d\n",*p);
 		printf(" i  is  %d\n",i);
+		break;
+	    }
 
-	}
-
-	// This is to demonstrate address arithmetic
-	if ( test == 4 ){
+	 case 4: {
+		// This is to demonstrate address arithmetic
 		int *p;
 		p = (int * ) 1024;  // careful! p itself is invalid :-)
-		printf("p holds the address %p\n", p);
+		printf("p holds the address %p\n", p); // note "%p" format conversion
 		printf("sizeof(int) is %ld\n", sizeof(int));
-		p ++;	// Note that p++ is pointer arithmetic:
-	       		//	it increases p to point to the integer location after the current integer address p.
+		p ++;	// Note that p++ is pointer arithmetic operation:
+	       		//	it increases p to point to the integer location 
+			//	after the current integer address p.
 		printf("p holds the address %p\n", p);
 		char *pc;
-		pc = (int *) 1024;
+		pc = (char *) 1024;
 		printf("p holds the address %p\n", pc);
 		printf("sizeof(char) is %ld\n", sizeof(char));
 		pc ++;	// Note that p++ is pointer arithmetic:
 	       		//	it increases p to point to the character location after the current character address p.
 		printf("p holds the address %p\n", pc);
 
-
-	} // what if we did p = p+2;
-
+		break; // what if we did p = p+2;
+	    }
+	} // end of the switch
 
 
 }
