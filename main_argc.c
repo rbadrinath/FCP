52bf26de85a41d88a350191e7a8aa711c36d2dc1
Badrinath R <r.badrinath@gmail.com>
2023-01-28 17:47:54 +0530

    update_main_argc

--- main_argc.c
+++ main_argc.c
@@ -3,5 +3,12 @@ int main(int argc, char * argv[]){
 	int i;
 	for ( i=0;argv[i]!=NULL;i++)
 		printf("%d %s\n",i,  argv[i]);
-	return i;
+
+	/*
+	for (i=0;i<argc;i++)
+		printf("%d %s\n",i,  argv[i]);
+	*/
+
+
+	return i; // return value from main() is the exit tatus
 }
