174043b2e40126025e1e1275c3420281ea44d221
Badrinath R <r.badrinath@gmail.com>
2023-11-04 11:18:51 +0530

    correctionBySriramSrikanth

--- tpcs.c
+++ tpcs.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 // this #include "tcps.h" declares the function prototype of test()
 //                   also  declares the global   extern  int num
-#include "tcps.h"
+#include "tpcs.h"
 
 int num= 0B00001000;
 int x=1;		// what if we made x to be static
